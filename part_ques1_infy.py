d5badfd7904b680e2fef9a3b2bc6625bc04a9c94
shahiarun48 <39365406+shahiarun48@users.noreply.github.com>
2018-11-18 21:19:42 +0530

    Create part_ques1_infy.py

---
+++ part_ques1_infy.py
@@ -0,0 +1,11 @@
+from nsepy import get_history
+from datetime import date
+import matplotlib.pyplot as plt
+data =get_history(symbol="INFY",start=date(2015,1,1),end=date(2016,1,31))
+
+def moving_average(wk):
+    ndays =wk*7
+    MA =pd.Series((data['Close']).rolling(window=ndays).mean(),name='wk'+'week moving average')
+    return data.join(MA)
+    
+moving_average(4)
